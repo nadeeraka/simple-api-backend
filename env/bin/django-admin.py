376cd99bf27482e75d582c06e222c398be43b9ae
nadeeraka <nadeerakan@gmail.com>
2020-01-18 23:06:57 +0530

    req add

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/nimantha/2A5628F45628C305/importent projects/pitaka/backend/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
