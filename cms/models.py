ae5809a96b52078d1d0c09e1e1e0d2e773f7e440
nadeeraka <nadeerakan@gmail.com>
2020-03-22 18:02:16 +0530

    modles adding

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['id']
+
+
+class Plant(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    site = models.ForeignKey(Site, on_delete=models.PROTECT)
+    admins = models.ManyToManyField(User, related_name='plant')
+
+    class Meta:
+        ordering = ['id']
+
