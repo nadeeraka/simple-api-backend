ff7690cdb7015bcaaa1a6ad962d65d4f7df01829
nadeeraka <nadeerakan@gmail.com>
2020-03-28 14:26:20 +0530

    serializers add

--- cms/serializers.py
+++ cms/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import routers, serializers, viewsets
 from django.contrib.auth.models import User, Group
 from cms import models
 
+
 # Serializers define the API representation.
 
 
@@ -21,3 +22,21 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Customer
         fields = ['id', 'code', 'name', 'email', 'details', 'ex_points']
+
+
+class ProductsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Products
+        fields = ['id', 'code', 'name', 'name', 'details', 'price', 'discount_rate', 'quantity']
+
+
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Store
+        fields = ['id', 'product_id', 'quantity', 'purchase_rate']
+
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Purchase
+        fields = ['id', 'customer_id', 'details', 'expenses', 'product_id', 'quantity']
