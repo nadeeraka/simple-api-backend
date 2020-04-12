e9b7a492b547ded83f30e3c56d44eb91661b558d
nadeeraka <nadeerakan@gmail.com>
2020-04-12 12:38:59 +0530

    views add

--- cms/views.py
+++ cms/views.py
@@ -4,8 +4,10 @@ from cms import serializers
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
+from cms import serializers, models
+from rest_framework import filters, pagination
 
-
+#framework
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -22,4 +24,59 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = serializers.GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+#custom models
 
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = models.Client.objects.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
+
+
+class IncomeViewSet(viewsets.ModelViewSet):
+    queryset = models.Income.objects.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
+
+
+class SavingViewSet(viewsets.ModelViewSet):
+    queryset = models.Saving.objects.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
+
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = models.Expense.objects.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
+
+
+class BalanceViewSet(viewsets.ModelViewSet):
+    queryset = models.Balance.objects.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
+
+
+class ExpenseTypeViewSet(viewsets.ModelViewSet):
+    queryset = models.Balance.ExpenseType.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
+
+
+class IncomeTypeViewSet(viewsets.ModelViewSet):
+    queryset = models.Balance.IncomeType.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
+
+
+class SavingTypeViewSet(viewsets.ModelViewSet):
+    queryset = models.Balance.SavingType.all()
+    serializer_class = serializers.SiteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination.PageNumberPagination.page_size_query_param = 'page_size'
