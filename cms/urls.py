8666791a417c8f89f553bb0c177e1a103a406b86
nadeeraka <nadeerakan@gmail.com>
2020-04-12 17:11:26 +0530

    bug fixed

--- cms/urls.py
+++ cms/urls.py
@@ -6,10 +6,19 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'clients', views.ClientViewSet)
+router.register(r'income', views.IncomeViewSet)
+router.register(r'incomeType', views.IncomeTypeViewSet)
+router.register(r'expense', views.ExpenseViewSet)
+router.register(r'expenseType', views.ExpenseTypeViewSet)
+router.register(r'saving', views.SavingViewSet)
+router.register(r'savingType', views.SavingTypeViewSet)
+router.register(r'BalanceType', views.BalanceViewSet)
+
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls'))
 ]
 1
